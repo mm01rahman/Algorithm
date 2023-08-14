d90c32fba745aaa72e29ea6f4feb8b7aaf87794f
mm01rahman <110207597+mm01rahman@users.noreply.github.com>
2023-08-14 09:09:57 +0600

    Update task2.cpp
    Arrray summation with reccursion

--- task2.cpp
+++ task2.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
-int sum(int *z,int start,int size,int total)
+int sum(int a[],int start,int sizes,int total)
 {
-    if(start<size)
+    if(start<sizes)
     {
-        *z = *(z+start);
-        total = total+ *z;
+        total += a[start];
         start++;
-        return sum(z,start,size,total);
+        return sum(a,start,sizes,total);
     }
     else
     {
@@ -17,10 +16,8 @@ int sum(int *z,int start,int size,int total)
 int main()
 {
     int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int *p;
-    int size = sizeof(a)/4;
-    p = &a[0];
-    int x = sum(p,0,size,0);
+    int sizes = sizeof(a)/sizeof(a[0]);
+    int x = sum(a,0,sizes,0);
     cout<<"The sum is :  "<<x;
     return 0;
 }
